00cc444ac3588ef1c10289a5df643a7d4c75a745
y3k0000 <y3k00000@github.com>
2019-04-01 16:09:09 +0800

    test addCallback

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-import {
-    TweenMax,
-    TimelineMax as Timeline
-} from 'gsap';
+import { TweenMax, TimelineMax as Timeline } from "gsap";
 
 TweenMax.set("#box", {
-    xPercent: -50,
-    yPercent: -50
+  xPercent: -50,
+  yPercent: -50
 });
 var timeline = new Timeline();
-document.addEventListener('click', ev => {
-    const {
-        clientX,
-        clientY
-    } = ev;
-    timeline.to("#box", 1, {
-        x: clientX,
-        y: clientY,
-        rotation: "+=90"
-    });
+var callbackIndex = 0;
+
+document.addEventListener("click", ev => {
+  console.log("callbackIndex=" + callbackIndex);
+  const { clientX, clientY } = ev;
+  timeline.to("#box", 1, {
+    x: clientX,
+    y: clientY,
+    rotation: "+=90"
+  });
+  timeline.addCallback(function() {
+    console.log("callback!!", (callbackIndex = callbackIndex + 1), arguments);
+  });
+});
+
+document.querySelector("#box").addEventListener("click", ev => {
+  TweenMax.killTweensOf(ev.target);
 });
